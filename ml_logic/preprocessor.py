c183eac252f6b01766fa5b57caae3f965e3afcf6
Malte Rudolph <rudolphmalte@gmail.com>
2023-05-27 17:51:45 +0200

    packeging

--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -7,14 +7,14 @@ from sklearn.preprocessing import OneHotEncoder
 def preprocess_features(df: pd.DataFrame) -> pd.DataFrame:
 
     df = df
-    
+
     ohe = OneHotEncoder(sparse = False)
     ohe.fit(df[['payment_format']])
     df[ohe.get_feature_names_out()] = ohe.transform(df[['payment_format']])
     #ohe.fit(df[['currency_pair']])
     #df[ohe.get_feature_names_out()] = ohe.transform(df[['currency_pair']])
 
-    
+
     print("✅ data preprocessed")
 
     return df
