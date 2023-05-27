46369cb5b87929f4e18f4d3187d042a8f6fc884a
Malte Rudolph <rudolphmalte@gmail.com>
2023-05-27 12:49:30 +0200

    preprcessing added

--- ml_logic/model.py
+++ ml_logic/model.py
@@ -18,11 +18,88 @@ print(f"\n✅ TensorFlow loaded ({round(end - start, 2)}s)")
 
 
 
-def initialize_model(input_shape: tuple) -> Model:
+def initialize_model(df) -> Model:
     """
     Initialize the model
     """
+    !pip install networkx
+    import networkx as nx
+    G = nx.MultiGraph()
 
+        # Add nodes to the graph for each unique card_id, merchant_name
+    G.add_nodes_from(df["Account"].unique(), type='Account')
+    G.add_nodes_from(df["Account.1"].unique(), type='Account.1')
+
+
+    for _, row in df_temp.iterrows():
+    # Create a variable for each properties for each edge
+    
+        year = row["Year"],
+        month = row["Month"],
+        day = row["Day"],
+        hour = row["Hour"],
+        minute =row["Minute"],
+        amount_paid = row["Amount Paid USD"],
+        payment_format_ach =  row["Payment Format_ACH"],
+        payment_format_bitcoin = row["Payment Format_Bitcoin"],
+        payment_format_cash = row["Payment Format_Cash"],
+        Payment_format_Cheque = row["Payment Format_Cheque"],
+        Payment_Format_Credit_Card = row["Payment Format_Credit Card"],
+        Payment_Format_Reinvestment = row["Payment Format_Reinvestment"],
+        Payment_Format_Wire =row["Payment Format_Wire"],
+        Currency_Code_BRL =  row["Currency Code_BRL"],
+        Currency_Code_BTC = row["Currency Code_BTC"],
+        Currency_Code_CHF = row["Currency Code_CHF"],
+        Currency_Code_EUR = row["Currency Code_EUR"],
+        Currency_Code_GBP = row["Currency Code_GBP"],
+        Currency_Code_ILS =row["Currency Code_ILS"],
+        Currency_Code_INR = row["Currency Code_INR"],
+        Currency_Code_JPY =  row["Currency Code_JPY"],
+        Currency_Code_MXN = row["Currency Code_MXN"],
+        Currency_Code_RUB =row["Currency Code_RUB"],
+        Currency_Code_SAR = row["Currency Code_SAR"],
+        Currency_Code_USD =  row["Currency Code_USD"]
+ 
+        G.add_edge(row['Account'], row['Account.1'], year = year , month = month , day = day ,
+              hour = hour , minute = minute , amount_paid = amount_paid, payment_format_ach =  payment_format_ach,
+              payment_format_bitcoin = payment_format_bitcoin, 
+            payment_format_cash = payment_format_cash,
+            Payment_format_Cheque = Payment_format_Cheque,
+            Payment_Format_Credit_Card = Payment_Format_Credit_Card,
+            Payment_Format_Reinvestment = Payment_Format_Reinvestment,
+            Payment_Format_Wire = Payment_Format_Wire,
+            Currency_Code_BRL =  Currency_Code_BRL,
+            Currency_Code_BTC = Currency_Code_BTC,
+            Currency_Code_CHF = Currency_Code_CHF,
+            Currency_Code_EUR = Currency_Code_EUR,
+            Currency_Code_GBP = Currency_Code_GBP,
+            Currency_Code_ILS = Currency_Code_ILS,
+            Currency_Code_INR = Currency_Code_INR,
+            Currency_Code_JPY =  Currency_Code_JPY,
+            Currency_Code_MXN = Currency_Code_MXN,
+            Currency_Code_RUB = Currency_Code_RUB,
+            Currency_Code_SAR = Currency_Code_SAR,
+            Currency_Code_USD =  Currency_Code_USD)
+        
+    # Get the number of nodes and edges in the graph
+    num_nodes = G.number_of_nodes()
+    num_edges = G.number_of_edges()
+
+    # Convert the graph to an adjacency matrix
+    adj_matrix = nx.adjacency_matrix(G).todense()
+    
+    import collections
+    # Retrieve the properties errors of all the edges
+    edge_properties = nx.get_edge_attributes(G, 'errors')
+
+    # Count the number of edges by property value
+    edge_count_by_property = collections.Counter(edge_properties.values())
+
+    # Print the count of edges by property value
+    for property_value, count in edge_count_by_property.items():
+    
+    print("Number of nodes:", num_nodes)
+    print("Number of edges:", num_edges)
 
     print("✅ Model initialized")
 
--- ml_logic/preprocessor.py
+++ ml_logic/preprocessor.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
 
-
-from sklearn.pipeline import make_pipeline
-from sklearn.compose import ColumnTransformer, make_column_transformer
-from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 
-def preprocess_features():
+def preprocess_features(df: pd.DataFrame) -> pd.DataFrame:
 
-    """ DUMMY """
+    df = df
+    
+    ohe = OneHotEncoder(sparse = False)
+    ohe.fit(df[['payment_format']])
+    df[ohe.get_feature_names_out()] = ohe.transform(df[['payment_format']])
+    #ohe.fit(df[['currency_pair']])
+    #df[ohe.get_feature_names_out()] = ohe.transform(df[['currency_pair']])
 
-    X_processed = preprocessor.fit_transform(X)
+    
+    print("✅ data preprocessed")
 
-    print("✅ X_processed, with shape", X_processed.shape)
-
-    return X_processed
+    return df
